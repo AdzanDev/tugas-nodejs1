6da879711b7bbfc3f6853b39127fa7df0716dd3f
int adzan <int-adzan@lalampa.me>
2023-05-09 20:07:21 +0700

    chore : myBiodata

---
+++ moduleBiodata.js
@@ -0,0 +1,11 @@
+const myBiodata = {
+   name: "Muhammad Adzan Nurkholiq",
+   placeOfBirth: "Bandung",
+   dateOfBirth: "26 Desember 2004",
+   addres: "JL.Ibu Sangki, Kota Cimahi",
+};
+
+console.log(`Name : ${myBiodata.name}`);
+console.log(`Tempat Lahir : ${myBiodata.placeOfBirth}`);
+console.log(`Tanggal Lahir : ${myBiodata.dateOfBirth}`);
+console.log(`Alamat : ${myBiodata.addres}`);
